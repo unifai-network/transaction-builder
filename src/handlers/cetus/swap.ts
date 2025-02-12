271022039bd909bf43acfbeab5724b3fb7b33fa8
0xBondSUI <sugar@mesolabs.io>
2025-02-12 19:28:35 +0800

    feat: add cetus

---
+++ src/handlers/cetus/swap.ts
@@ -0,0 +1,89 @@
+import { z } from 'zod';
+import { TransactionHandler, CreateTransactionResponse, BuildTransactionResponse } from "../TransactionHandler";
+import { suiClient, validateSuiAddress } from '../../utils/sui'
+import { Transaction } from '@mysten/sui/transactions'
+import { AggregatorClient, Env } from '@cetusprotocol/aggregator-sdk'
+
+const PayloadSchema = z.object({
+  from: z.string().nonempty("Missing required field: from"),
+  target: z.string().nonempty("Missing required field: target"),
+  amount: z.number().positive("Amount must be a positive number"),
+  slippage: z.number().min(0).max(1).default(0.005), // 0.5%
+});
+
+type Payload = z.infer<typeof PayloadSchema>;
+
+export class SwapHandler implements TransactionHandler {
+  async create(payload: Payload): Promise<CreateTransactionResponse> {
+    const validation = PayloadSchema.safeParse(payload);
+
+    if (!validation.success) {
+      throw new Error(validation.error.errors.map(e => `${e.path.join('.')}: ${e.message}`).join(', '));
+    }
+
+    payload = validation.data;
+
+    validateSuiAddress(payload.from);
+    validateSuiAddress(payload.target);
+
+    return {
+      chain: "sui",
+      data: {
+        from: payload.from,
+        target: payload.target,
+        amount: payload.amount,
+      },
+    };
+  }
+
+  async build(data: Payload, wallet: string): Promise<BuildTransactionResponse> {
+    const cetusClient = new AggregatorClient(undefined, wallet, suiClient, Env.Mainnet)
+
+    const fromCoinMetadata = await suiClient.getCoinMetadata({
+      coinType: data.from,
+    })
+
+    if (!fromCoinMetadata) {
+      throw new Error(`Coin metadata not found for ${data.from}`);
+    }
+
+    const amount = (data.amount * (10 ** fromCoinMetadata.decimals)).toFixed(0);
+
+    // true means fix input amount, false means fix output amount, default use true.
+    const byAmountIn = true;
+
+    const routerRes = await cetusClient.findRouters({
+      from: data.from,
+      target: data.target,
+      amount,
+      byAmountIn,
+    })
+
+    if (!routerRes) {
+      throw new Error(`No router found for ${data.from} to ${data.target}`);
+    }
+
+    const txb = new Transaction()
+
+    await cetusClient.fastRouterSwap({
+      routers: routerRes,
+      slippage: data.slippage,
+      txb,
+      refreshAllCoins: true,
+    })
+
+    txb.setSender(wallet)
+
+    const txBytes = await txb.build({
+      client: suiClient
+    })
+
+    const txBase64 = Buffer.from(txBytes).toString('base64')
+
+    return {
+      transactions: [{
+        base64: txBase64,
+      }],
+    };
+  }
+}

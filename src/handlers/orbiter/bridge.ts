c5b0a6a1e08930ab08bce586a9cdd1e61b6ca4d0
notctrl <97250108+notctrl3@users.noreply.github.com>
2025-04-25 20:57:20 +0800

    Update bridge.ts

--- src/handlers/orbiter/bridge.ts
+++ src/handlers/orbiter/bridge.ts
@@ -1,8 +1,12 @@
 import { z } from "zod";
 import { BuildTransactionResponse, CreateTransactionResponse, TransactionHandler } from "../TransactionHandler";
-import { OrbiterClient, ENDPOINT, RouterType, ConfigOptions, TradePair } from "@orbiter-finance/bridge-sdk";
-import { EVM_CHAIN_IDS, getEvmProvider, validateEvmAddress, validateEvmChain } from "../../utils/evm";
+import { OrbiterClient, ENDPOINT, RouterType, ConfigOptions, TradePair, Router } from "@orbiter-finance/bridge-sdk";
+import { EVM_CHAIN_IDS, getEvmProvider, checkERC20Balance, validateEvmChain } from "../../utils/evm";
+import { validateAddress } from "../../utils/validators";
 import { ethers } from "ethers";
+import { connection } from "../../utils/solana";
+import { PublicKey, TransactionMessage, VersionedTransaction, TransactionInstruction} from '@solana/web3.js';
+
 
 const config: ConfigOptions = {
   apiEndpoint: ENDPOINT.MAINNET,
@@ -11,7 +15,8 @@ const config: ConfigOptions = {
 
 const PayloadSchema = z.object({
   srcChain: z.string().nonempty("Missing required field: srcChain"),
-  dstChain: z.string().nonempty("Missing required field: dstChain"),
+  dstChain: z.string().nonempty("Missing required field: dstChain"), 
+  dstAddress: z.string().nonempty("Missing required field: dstAddress"),
   srcTokenSymbol: z.string().toUpperCase().nonempty("Missing required field: srcTokenSymbol"),
   dstTokenSymbol: z.string().toUpperCase().nonempty("Missing required field: dstTokenSymbol"),
   amount: z.string().regex(/^\d+(\.\d+)?$/, "Amount must be a valid number string"),
@@ -29,8 +34,8 @@ export class OrbiterHandler implements TransactionHandler {
     }
 
     payload = validation.data;
-    validateEvmChain(payload.srcChain);
-    validateEvmChain(payload.dstChain);
+    if(payload.srcChain !== 'solana') validateEvmChain(payload.srcChain);
+    validateAddress(payload.dstChain, payload.dstAddress);
 
     const orbiter = await OrbiterClient.create(config);
 
@@ -43,16 +48,33 @@ export class OrbiterHandler implements TransactionHandler {
   }
 
   async build(data: Payload, address: string): Promise<BuildTransactionResponse> {
-    validateEvmAddress(address);
-
-    const provider = getEvmProvider(data.srcChain);
-
     const orbiter = await OrbiterClient.create(config);
 
     const tradePair = getTradePair(orbiter, data.srcChain, data.srcTokenSymbol, data.dstChain, data.dstTokenSymbol);
 
     const router = orbiter.createRouter(tradePair);
+    
+    const min = Number(router.getMinSendAmount());
+    const max = Number(router.getMaxSendAmount());
 
+    if (Number(data.amount) < min || Number(data.amount) > max) {
+      throw new Error(`Amount: ${data.amount} must be between ${min} and ${max}`);
+    }
+
+    let res = null;
+    if(data.srcChain === 'solana') {
+      res = await this.generateTxSolona(data, address, router);
+    } else {
+      res = await this.generateTxEvm(data, address, router)
+    }
+    return res;
+  }
+
+  async generateTxEvm(data: Payload, address: string, router: Router) : Promise<BuildTransactionResponse>{
+    const transactions: Array<{ hex: string }> = [];
+
+    const provider = getEvmProvider(data.srcChain);
+ 
     const min = Number(router.getMinSendAmount());
     const max = Number(router.getMaxSendAmount());
 
@@ -62,14 +84,6 @@ export class OrbiterHandler implements TransactionHandler {
 
     const { sendAmount } = router.simulationAmount(data.amount);
 
-    const transaction = await router.createTransaction(address, address, sendAmount);
-    
-    const rawData = transaction.raw as {
-      to: string;
-      data: string;
-      value: string;
-    };
-
     const feeData = await provider.getFeeData();
 
     const { maxFeePerGas, maxPriorityFeePerGas } = feeData;
@@ -78,6 +92,47 @@ export class OrbiterHandler implements TransactionHandler {
       throw new Error("Missing fee data");
     }
 
+    // make sure the token is an erc-20 token. if so, request approval before the transaction
+    if(data.srcTokenSymbol.toUpperCase() === 'USDC') {
+      const { isEnough } = await checkERC20Balance(provider,
+        data.srcChain,
+        address,
+        data.amount);
+      if(!isEnough) throw new Error(`transfer amount exceeds balance for ${data.srcChain} 
+        ${data.srcTokenSymbol}`);
+      
+      const approveTransaction = await router.createApprove(address, 
+        sendAmount);
+      const approveRawData = approveTransaction.raw as {
+        to: string;
+        data: string;
+        value: string;
+      };
+
+      const unsignedApproveTx: ethers.TransactionLike = {
+        chainId: EVM_CHAIN_IDS[data.srcChain],
+        type: 2,
+        to: approveRawData.to,
+        data: approveRawData.data,
+        value: approveRawData.value || '0x0',
+        maxFeePerGas: maxFeePerGas,
+        maxPriorityFeePerGas: maxPriorityFeePerGas,
+      };
+
+      transactions.push({ hex: ethers.Transaction.from(unsignedApproveTx).unsignedSerialized });
+    }
+    
+
+    const transaction = await router.createTransaction(address,
+      data.dstAddress, sendAmount);
+    
+    const rawData = transaction.raw as {
+      to: string;
+      data: string;
+      value: string;
+    };
+
+
     const unsignedTx: ethers.TransactionLike = {
       chainId: EVM_CHAIN_IDS[data.srcChain],
       type: 2,
@@ -85,27 +140,57 @@ export class OrbiterHandler implements TransactionHandler {
       maxFeePerGas: maxFeePerGas,
       maxPriorityFeePerGas: maxPriorityFeePerGas,
     };
-    const serializedTx = ethers.Transaction.from(unsignedTx).unsignedSerialized;
+    transactions.push({ hex: ethers.Transaction.from(unsignedTx).unsignedSerialized });
 
-    return {
-      transactions: [
-        {
-          hex: serializedTx,
-        },
-      ],
-    };
+    return {transactions};
   }
+
+  async generateTxSolona(data: Payload, address: string, router: Router) : Promise<BuildTransactionResponse>{
+    const sender = new PublicKey(address);
+    const { sendAmount } = router.simulationAmount(data.amount);
+    const transfers = await router.createTransaction(address, data.dstAddress, sendAmount);
+    console.log(transfers);
+    const rawData = transfers.raw as {
+      instructions: Array<TransactionInstruction>;
+    };
+
+    const blockhash = await connection.getLatestBlockhash();
+    
+    const messageV0 = new TransactionMessage({
+        payerKey: sender,
+        recentBlockhash: blockhash.blockhash,
+        instructions: rawData.instructions,
+    }).compileToV0Message();
+    const transaction = new VersionedTransaction(messageV0);
+    return {
+      transactions: [{
+        type: 'versioned',
+        base64: Buffer.from(transaction.serialize()).toString('base64')
+      }]
+    }
+  }
+    
 }
 
-function getTradePair(orbiter: OrbiterClient, srcChain: string, srcTokenSymbol: string, dstChain: string, dstTokenSymbol: string): TradePair {
-  const tradePairs: TradePair[] = orbiter.getAvailableTradePairs(EVM_CHAIN_IDS[srcChain].toString(), srcTokenSymbol);
+
+
+function getTradePair(orbiter: OrbiterClient, 
+  srcChain: string, 
+  srcTokenSymbol: string, 
+  dstChain: string, 
+  dstTokenSymbol: string): TradePair 
+  {
+  const srcChainId = srcChain === 'solana' ? 'SOLANA_MAIN' : EVM_CHAIN_IDS[srcChain].toString();
+  const dstChainId = dstChain === 'solana' ? 'SOLANA_MAIN' : EVM_CHAIN_IDS[dstChain].toString();
+
+  const tradePairs: TradePair[] = orbiter.getAvailableTradePairs(srcChainId, srcTokenSymbol);
 
   if (tradePairs.length === 0) {
     throw new Error(`No trade pairs found for ${srcChain} ${srcTokenSymbol}`);
   }
 
   const tradePair = tradePairs.find(
-    (pair) => pair.dstChainId === EVM_CHAIN_IDS[dstChain].toString() && pair.dstTokenSymbol === dstTokenSymbol
+    (pair) => pair.dstChainId === dstChainId && pair.dstTokenSymbol === dstTokenSymbol
   );
 
   if (!tradePair) {
@@ -116,3 +201,5 @@ function getTradePair(orbiter: OrbiterClient, srcChain: string, srcTokenSymbol: 
 
   return tradePair;
 }
+
+
